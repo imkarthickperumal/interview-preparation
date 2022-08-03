85f29061a1067ebc675907af1a287194a455a2e8
imkarthi16 <karthiveera@gmail.com>
2022-08-03 08:26:24 +0530

    question no:3 Event Deligation

--- unacademy-interview/script.js
+++ unacademy-interview/script.js
@@ -13,10 +13,10 @@ const forEachResult = arr.forEach((ar)=>{
 console.log(mapResult, forEachResult);
 
 
-// Qestion 2 : null vs undefined
+// Question 2 : null vs undefined
 let a ;
 let b = null;
-console.log(a); // variable declared n but not assign value
+console.log(a); // variable declared but not assign value
 console.log(b); // variable declared but value of null(obj)
 
 console.log(typeof(null));
@@ -24,5 +24,14 @@ console.log(typeof(undefined));
 
 console.log(null==undefined); // true => check both of the types anyone is true the result is true
 console.log(null===undefined); // false => check both of the types
+
+
+// Question 3 : Event Deligation
+document.querySelector("#products").addEventListener("click",(event)=>{
+    console.log(event);
+    if(event.target.tagName === 'LI'){
+        window.location.href += "#" + event.target.id;
+    }
+})
 
 
